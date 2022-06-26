edcd7334636609ae4928e20555e7b7b5ac805e55
laureanray <laureanraybahala@gmail.com>
2022-06-26 18:21:58 +0800

    added file size to struct and output

--- pkg/api/book.go
+++ pkg/api/book.go
@@ -1,11 +1,12 @@
 package api
 
 type Book struct {
-	ID        string   `json:"ID"`
-	Title     string   `json:"title"`
-	Author    string   `json:"author"`
-	Year      string   `json:"year"`
-	Publisher string   `json:"publisher"`
-	Extension string   `json:"extension"`
-	Mirrors   []string `json:"mirrors"`
+	ID        string
+	Title     string
+	Author    string
+	Year      string
+	Publisher string
+	Extension string
+	Mirrors   []string
+	FileSize  string
 }
