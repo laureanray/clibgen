05ec0dc351afc619036eb214816621993e7435f6
laureanray <laureanraybahala@gmail.com>
2022-07-13 14:24:08 +0800

    clean up code, reorganize imports

--- cmd/search.go
+++ cmd/search.go
@@ -2,10 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/laureanray/clibgen/pkg/api"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 var (
@@ -17,16 +18,13 @@ var (
 		Short: "search for a book, paper or article",
 		Long: `search for a book, paper or article
 	example: clibgen search "Eloquent JavaScript"`,
-		Run: func(cmd *cobra.Command, args []string) {
+		Run: func(_ *cobra.Command, args []string) {
 			if len(args) == 0 {
 				fmt.Println("Please enter a search query!")
 				return
 			}
 
-			//var stringFlag = flag.String("mirror", "m", "select which mirror to use (old/new) [libgen.is, libgen.li]")
 			var libgenType = api.LibgenNew
-
-			//fmt.Println(&stringFlag)
 
 			if selectedMirror == "old" {
 				libgenType = api.LibgenOld
