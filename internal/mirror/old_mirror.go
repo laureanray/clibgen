542246a0e797e0ac6f6f7f8ebcd5b47bf65eaffa
laureanray <laureanraybahala@gmail.com>
2023-06-24 00:49:33 +0800

    change domain to libgen

--- internal/mirror/old_mirror.go
+++ internal/mirror/old_mirror.go
@@ -1,6 +1,9 @@
 package mirror
 
 import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
 	"github.com/laureanray/clibgen/internal/book"
 	"github.com/laureanray/clibgen/internal/domain"
 )
@@ -26,7 +29,29 @@ func (m *OldMirror) SearchByTitle(query string) []book.Book {
 
 // Search the libgen site returns the document 
 // of the search results page
-func (m *OldMirror) searchSite() {
+func (m *OldMirror) searchSite() (*goquery.Document, error) {
+  baseUrl := fmt.Sprintf("https://libgen.%s/search.php", m.domain)
+	queryString := fmt.Sprintf("%s?req=%s&res=25&view=simple&phrase=1&column=%s", baseUrl, url.QueryEscape(query))
+	resp, e := http.Get(queryString)
 
+	if e != nil {
+		return nil, e
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			e = err
+		}
+	}(resp.Body)
+
+	document, e = goquery.NewDocumentFromReader(resp.Body)
+
+	if e != nil {
+		fmt.Println(e)
+		return nil, e
+	}
+
+	return document, e
 }
 
